3814ebb6d287280f1c86284e2d7bbb95b6665dc4
housten <housten@gmail.com>
2022-10-11 13:05:09 +0200

    Update create-workflow-pr.sh

--- .github/script/create-workflow-pr.sh
+++ .github/script/create-workflow-pr.sh
@@ -18,12 +18,6 @@ echo "Where are we:"
 pwd
 ls
 
-echo "Make sure we are on the main branch"
-git checkout main
-
-echo "Create new branch or checkout if it exists"
-git switch -C $PR_BRANCH
-
 echo "Copy example workflow into .github/workflows/"
 cp .github/example-workflows/$WORKFLOW_FILE .github/workflows/
 
@@ -32,10 +26,10 @@ sed -r "s/<username>/$REPO_OWNER/g" .github/workflows/$WORKFLOW_FILE > tmp
 mv tmp .github/workflows/$WORKFLOW_FILE
 
 echo "Commit the file, and push to new branch"
-git config user.name github-actions
-git config user.email github-actions@github.com
+
 git add .github/workflows/$WORKFLOW_FILE
 git commit --message="Added $WORKFLOW_FILE"
+git pull
 git push origin $PR_BRANCH
 echo "Create pull request for $PR_BRANCH into main"
 gh pr create --base main --head $PR_BRANCH --title "$PR_TITLE" --body ""
